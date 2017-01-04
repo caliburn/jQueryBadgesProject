84275e4bc7236f9ddb5589ea94d0e1031d678178
Allen T. Brown <allentbrown@gmail.com>
2017-01-04 10:28:16 -0500

    Updated to display completed course badges using ajax and jQuery

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,38 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/caliburn.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      addCourses(response.courses.completed);
+    }
+  });
+
+  function addCourses(courses) {
+
+    var $badges = $('#badges');
+
+    courses.forEach(function(course) {
+
+      var $course = $('<div />', {
+        'class': 'course'
+      }).appendTo($badges);
+
+      $('<h3>' + course.title + '</h3>').appendTo($course);
+
+      $('<img />', {
+        'src': course.badge
+      }).appendTo($course);
+
+      $('<a />', {
+        'href': course.url,
+        'target': '_blank',
+        'class': 'btn btn-primary',
+        text: 'See Course'
+      }).appendTo($course);
+
+    })
+
+  }
 
 });
